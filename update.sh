33465323b9eea2536056cbcc3b4a2ac8c764d25d
karia <karia@side2.net>
2016-02-12 01:53:36 +0900

    first commit

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+if [ $# -ne 1 ] ; then
+  echo "usage: $0 /path/tp/chinachu-dir"
+  exit
+fi
+
+# set PATH for execution by crontab
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+echo $PATH
+
+cd $1
+./chinachu update -f
+
+exit
+
